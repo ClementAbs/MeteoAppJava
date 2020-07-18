4ab4dde26e5f098c3b9c51fdbbe7310f92a43c28
clément <clement.abs@gmail.com>
2020-07-18 19:00:41 +0200

    JavaSwing

---
+++ src/com/clementabs/weatherapp/MainFrame.java
@@ -0,0 +1,16 @@
+package com.clementabs.weatherapp;
+
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8932567605745713844L;
+
+	public MainFrame(String title) {
+		super(title);
+	}
+
+}
